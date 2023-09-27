43064b363bc6b05fbb89786b27d55d45a44a3443
faishalrizqi <145439817+faishalrizqi@users.noreply.github.com>
2023-09-27 12:22:43 +0700

    Jurnal02.java

--- Jurnal02.java
+++ Jurnal02.java
@@ -1,7 +1,7 @@
 public class Jurnal02{
     public static void main(String[] args) {
         System.out.println("Nama  : Muhammad Faishal Rizqi Pratama");
-		System.out.println("NIM   : 607062300094");
-		System.out.println("Kelas : D3IF-47-03");
+	System.out.println("NIM   : 607062300094");
+	System.out.println("Kelas : D3IF-47-03");
     }
 }
